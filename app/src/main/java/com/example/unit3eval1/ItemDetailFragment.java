c5e715a2a73fc5e65b66f4657d4b867fae2c8b8b
dudi111 <pd.rajiv.000@gmail.com>
2021-08-09 14:41:44 +0530

    commit2

---
+++ app/src/main/java/com/example/unit3eval1/ItemDetailFragment.java
@@ -0,0 +1,22 @@
+package com.example.unit3eval1;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class ItemDetailFragment extends Fragment {
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_item_detail, container, false);
+    }
+}
